ec9410df1c82e8b17346564dbdb4aa0be9f2ef6a
feuvpi <fredvpg@gmail.com>
2023-04-28 00:15:56 -0300

    configuring register form and product list.

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+    <Products />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '@/components/Navbar.vue'
+import Tutorial from '@/components/Tutorial.vue'
+import Products from '@/pages/products/index.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    Navbar,
+    Products,
+    Tutorial
+  }
+})
+</script>
+
